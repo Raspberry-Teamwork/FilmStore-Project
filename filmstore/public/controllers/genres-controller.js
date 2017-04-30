46bddc11951c19e3e55e6bc00e57a09dfb73138b
marvasgit <marin.vassilev@gmail.com>
2017-05-01 02:04:42 +0300

    some Errors
    fix

--- filmstore/public/controllers/genres-controller.js
+++ filmstore/public/controllers/genres-controller.js
@@ -1,28 +1,31 @@
-import { requester } from 'requester';
-import { genreService } from 'genres-service';
+import {
+  requester
+} from 'requester';
+import {
+  genreService
+} from 'genres-service';
 
 
 class GenresController {
   constructor() {}
 
-   loadGenreMovie(sammy) {
+  loadGenreMovie(sammy) {
 
     let title = sammy.path.split('/')[3],
-        compile;
-
-    console.log(title);
-     requester.get('./views/templates/genres-movie-template.handlebars')
-             .then((template) => {
-               genreService.getMovieByGenres(title)
-                            .then((movies) => {
-                              compile = Handlebars.compile(template);
-                              $('#main-content').html(compile(movies));
-                            })
-                            .catch(console.log);
-             });
+      compile;
+    requester.get('./views/templates/genres-movie-template.handlebars')
+      .then((template) => {
+        genreService.getMovieByGenres(title)
+          .then((movies) => {
+            compile = Handlebars.compile(template);
+            $('#main-content').html(compile(movies));
+          })
+          .catch(console.log);
+      });
   }
 }
 
 const genresController = new GenresController();
-
-export { genresController };
+export {
+  genresController
+};
