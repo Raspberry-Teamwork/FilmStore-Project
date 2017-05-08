b90059192ae5224778d4f36f7eaf879f3844b517
marvasgit <marin.vassilev@gmail.com>
2017-05-08 16:57:41 +0300

    DatePicker
    das

--- filmstore/public/services/movies-service.js
+++ filmstore/public/services/movies-service.js
@@ -29,7 +29,7 @@ const ERROR_MESSAGE = {
    WRONG_IMG_URL: 'Wrong img url. It must begin with http or https.',
 
   WRONG_IMDB_ID: "The given ID isn't found or it is incorrect."
-};
+}
 
 class MoviesService {
   constructor() {}
@@ -98,17 +98,12 @@ class MoviesService {
         }
           const values = Object.keys(movies[0]);
           const props={};
-          //changing format of object properties for easier handling with templates
-          values.forEach(function(element) {
-            if(element=='Genre'){
-            props[element]={genre:element};
-            }else{
-            props[element]=element;
-            }
-          }, this);
-          let result={keys:props};
 
-            resolve(result);
+          values.forEach(function(element) {
+            props[element]=element;
+          }, this);
+console.log(props);
+            resolve(props);
       });
 
     });
@@ -157,7 +152,7 @@ class MoviesService {
       validator.isEmpty(movie.TrailerUrl, ERROR_MESSAGE.EMPTY_TRAILER_URL);
       validator.validateUrl(movie.TrailerUrl, ERROR_MESSAGE.WRONG_TRAILER_VIDEO_URL, ['https', 'youtube']);
 
-      validator.validateUrl(movie.Poster, ERROR_MESSAGE.WRONG_IMG_URL, ['http', 'https']);
+      validator.validateUrl(movie.Poster, ERROR_MESSAGE.WRONG_IMG_URL, ['http', 'http']);
     } catch(error) {
       return Promise.reject({ message: error.message });
     }
--- filmstore/public/style/style.js
+++ filmstore/public/style/style.js
@@ -1,3 +1,21 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+userController.onAuthStateChanged(function(user) {
+  if (user) {
+    $('.browse').removeClass('invisible');
+    $('#singin').addClass('invisible');
+    $('.signup').addClass('invisible');
+    $('.account').removeClass('invisible');
+    $('#add-trailer').removeClass('invisible');
+
+    userController.showAccount(user);
+
+    console.log('logged');
+  } else {
+    $('.browse').addClass('invisible');
+    $('#add-trailer').addClass('invisible');
+    $('#singin').removeClass('invisible');
+    $('.signup').removeClass('invisible');
+    $('.account').addClass('invisible');
+
+    console.log('not logged');
+  }
+});
